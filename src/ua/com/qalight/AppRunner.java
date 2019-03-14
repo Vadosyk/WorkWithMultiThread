4c2acec1fbb99bdee4bff1e77421650cecb0a829
Vados <Vados@DESKTOP-FF9CVKD>
2019-03-14 23:42:06 +0200

    Work With MultiThread

---
+++ src/ua/com/qalight/AppRunner.java
@@ -0,0 +1,7 @@
+package ua.com.qalight;
+
+public class AppRunner {
+	
+	
+
+}
---
+++ src/ua/com/qalight/services/MultithreadServices.java
@@ -0,0 +1,10 @@
+package ua.com.qalight.services;
+
+public class MultithreadServices {
+	
+	private final String threadName;
+	public MultithreadService (String threadName) {
+		this.threadName = threadName;
+	}
+
+}
