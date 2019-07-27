bc8bdf69b5158c3ccce0fa6f60cc93719c25f996
Vadym <vadosyk@yahoo.com>
2019-07-27 20:31:55 +0300

    mistakes

--- src/ua/kiev/prog/MultiThreadCopyFile/Main.java
+++ src/ua/kiev/prog/MultiThreadCopyFile/Main.java
@@ -3,7 +3,7 @@ package ua.kiev.prog.MultiThreadCopyFile;
 import java.io.File;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 	     
 		 final int NUMBERS_OF_BLOCKS = 5;
 		 	String source = "D:\\Test1";
@@ -12,9 +12,17 @@ public class Main {
 			File dest = new File(destination);
 			
        for (int i = 0; i <NUMBERS_OF_BLOCKS ; i++) { 	   
-    	   MultiThreadCopyFile thred = new MultiThreadCopyFile(src, dest, src.length()*i/NUMBERS_OF_BLOCKS, (src.length()*(i+1)/NUMBERS_OF_BLOCKS));
-    	   thred.start();
-               
+    	   MultiThreadCopyFile thread = new MultiThreadCopyFile(src, dest, src.length()*i/NUMBERS_OF_BLOCKS, (src.length()*(i+1)/NUMBERS_OF_BLOCKS));
+    	   thread.start();
+            
+    	  /* try {
+               thread.join();                   
+               System.out.println(thread.getName()); 
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }*/
+    	   
+    	   System.out.println(thread.getName());
         }
        
 	}
